fe52bddb1a064169273e11b720b6fe56f5293195
acanmert <ahmetcanmert25@gmail.com>
2024-05-04 19:41:24 +0300

    last

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = Flask(__name__)
 
 def secimlik(secim):
-    data = pd.read_csv(f'C:\\Users\\AhmetCan\\source\\repos\\File_Upload\\File_Upload\\wwwroot\\dataset\\{secim}', encoding='ISO-8859-9')
+    data = pd.read_csv(f'C:\\Users\\AhmetCan\\source\\repos\\File_Upload\\File_Upload\\wwwroot\\dataset\\{secim}', encoding='"utf-8-sig')
     return  data
 
 def create_combined_features(row, selected_features):
     return ' '.join([str(row[feature]) for feature in selected_features])
 
-def get_recommendations(title, data, selected_features,p_name,p_type):
+def get_recommendations(title, data, selected_features,p_name,p_type, top_n=10,):
     try:
         # Kullanıcının seçtiği özelliklere göre 'combined_features' sütununu oluştur
         data['combined_features'] = data.apply(lambda row: create_combined_features(row, selected_features), axis=1)
@@ -28,9 +28,13 @@ def get_recommendations(title, data, selected_features,p_name,p_type):
             cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
             similarity_scores = list(enumerate(cosine_sim[index]))
             similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-            top_recommendations = similarity_scores[1:11]
-            recommended = [data[p_name][top_data[0]] for top_data in top_recommendations]
-            return recommended
+            top_recommendations = similarity_scores[1:top_n + 1]
+
+            recommended_rows = []
+            for top_data in top_recommendations:
+                recommended_rows.append(data.iloc[top_data[0]])
+
+            return recommended_rows #, [score[1] for score in top_recommendations]
         else:
             return ["No recommendation available"]  # veya başka bir hata durumu işleme
     except Exception as e:
@@ -46,7 +50,7 @@ def get_recommendations_endpoint():
     p_type=request.args.get("p_type")
     recommendations = get_recommendations(title, data, selected_features,p_name,p_type)
 
-    return     str(recommendations)
+    return str(recommendations)
 
 if __name__ == '__main__':
     app.run(debug=True)
